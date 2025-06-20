3f5abab4b85e494f1abfd9181f29814737c6cc15
Jalzate257 <malzatej257@gmail.com>
2025-06-20 00:40:53 -0500

    Implementa funcionalidad nodeMailer

---
+++ nodeMailer/index.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+
+const emails = [
+    'johnortega1357@gmail.com'
+];
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'johnortega13579@gmail.com',
+        pass: 'bgsc lmpt idem godl',
+    },
+});
+
+async function sendMassEmails() {
+    const sendEmail = async (to) => {
+        try {
+            const info = await transporter.sendMail({
+                from: '"John Doe" <tucorreo@gmail.com>', 
+                to,
+                subject: 'Correo Masivo',
+                text: 'Este es un correo masivo enviado con Node.js',
+                html: '<b>Hola!</b> Este correo fue enviado masivamente.',
+            });
+            console.log(`Correo enviado a ${to}: ${info.messageId}`);
+        } catch (err) {
+            console.error(`Error con ${to}: ${err.message}`);
+        }
+    };
+
+    await Promise.all(emails.map(sendEmail));
+}
+
+sendMassEmails();
